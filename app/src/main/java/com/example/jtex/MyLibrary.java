5148d7bca2cdb611d2bfb665c00088eb937bb862
greatdushal198 <greatdushal198@gmail.com>
2023-04-12 17:56:20 +0530

    new library

---
+++ app/src/main/java/com/example/jtex/MyLibrary.java
@@ -0,0 +1,5 @@
+package com.example.jtex;
+
+public class MyLibrary {
+    // empty class, just a placeholder for the library
+}
---
+++ app/src/main/java/com/example/jtex/MyUtils.java
@@ -0,0 +1,7 @@
+package com.example.jtex;
+
+public class MyUtils {
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+}
